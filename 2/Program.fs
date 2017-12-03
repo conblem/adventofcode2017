0f778e338b8365e03a332259f1d137b2dfc9f42b
conblem <mail@conblem.me>
2017-12-03 21:16:09 +0100

    Second challenge

---
+++ 2/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+let split (x: String) = 
+    x.Split [|'\t'|]
+    |> List.ofArray
+    |> List.map int
+
+let cheksum x =
+    let min = List.min x
+    let max = List.max x
+    max - min
+
+[<EntryPoint>]
+let main argv =
+    let result = 
+        File.ReadAllLines "./2/input.txt"
+        |> List.ofArray
+        |> List.map split
+        |> List.map cheksum
+        |> List.sum
+    printfn "%i" result
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
