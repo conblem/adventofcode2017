33509d13973f12de6d71148375fe2cd52ec36c82
conblem <mail@conblem.me>
2017-12-04 10:46:49 +0100

    Fourth challenge

---
+++ 4/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+let split (elem: String) = elem.Split [|' '|] |> List.ofArray
+let distinct elem =
+    elem
+    |> List.distinct
+    |> (=) elem
+
+[<EntryPoint>]
+let main argv =
+    let result = 
+        "./4/input.txt"
+        |> File.ReadAllLines
+        |> List.ofArray
+        |> List.map split
+        |> List.filter distinct
+        |> List.length
+
+    printfn "%i" result
+    System.Console.ReadKey () |> ignore
+    0 // return an integer exit code
