c101b397ec55a74660b175bf340e56e355109b2b
conblem <mail@conblem.me>
2017-12-04 10:47:24 +0100

    WIP third challenge

---
+++ 3/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+let input = 325489 |> float
+
+[<EntryPoint>]
+let main argv =
+    let size = input |> sqrt |> ceil |> int
+    let ringMax = pown size 2
+    printfn "%i %i" size ringMax
+    System.Console.ReadKey () |> ignore
+    0 // return an integer exit code
