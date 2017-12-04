bc70001cde1a93653e8316bb30e806f5b6531a9e
conblem <mail@conblem.me>
2017-12-04 13:41:08 +0100

    Small cleanup in first challenge

--- 1/Program.fs
+++ 1/Program.fs
@@ -2,17 +2,22 @@
 
 let charToInt char = char.ToString () |> int
 
-let compute acc elem =
-    let (last, total) = acc
+let compute (last, total) elem =
     if elem = last
         then (elem, total + elem)
         else (elem, total)
 
 [<EntryPoint>]
 let main argv =
-    let input = File.ReadAllText "./1/input.txt" |> List.ofSeq |> List.map charToInt
+    let input =
+        "./1/input.txt"
+        |> File.ReadAllText
+        |> List.ofSeq
+        |> List.map charToInt
+
     let last = List.last input
     let (_, result) = List.fold compute (last, 0) input
+
     printfn "%i" result
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
