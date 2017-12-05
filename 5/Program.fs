c941f04857a2a9b968ea3dd5bdd415c109bc6e7b
conblem <mail@conblem.me>
2017-12-05 06:53:40 +0100

    WIP challenge 5

---
+++ 5/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+let replace (list: 'T []) index value =
+    Array.set list index value
+    list
+
+let rec iterate list index =
+    match List.tryItem index with
+    | Some item -> replace(list index item)
+    | None ->
+
+Array.set
+
+[<EntryPoint>]
+let main argv =
+    let input =
+        "./input.txt"
+        |> File.ReadAllLines
+        |> List.ofArray
+        |> List.map int
+    
+    printfn "%A" input
+    System.Console.ReadKey |> ignore
+    0 // return an integer exit code
