1d2e6080840ed0576a4dfa042d8e071b10273a7f
conblem <mail@conblem.me>
2017-12-05 08:08:21 +0100

    Fourth challenge second part

---
+++ 04 2/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.IO
+
+let split (elem: String) = elem.Split [|' '|] |> List.ofArray
+let distinct (elem: List<string>) =
+    elem
+    |> List.map Seq.sort
+    |> List.map System.String.Concat
+    |> List.distinct
+    |> List.length
+    |> (=) elem.Length
+
+[<EntryPoint>]
+let main argv =
+    let result = 
+        "./04 2/input.txt"
+        |> File.ReadAllLines
+        |> List.ofArray
+        |> List.map split
+        |> List.filter distinct
+        |> List.length
+
+    printfn "%i" result
+    System.Console.ReadKey () |> ignore
+    0
