e91027cb45d4b23199afd19a4bc6a80d061dfa85
conblem <mail@conblem.me>
2017-12-05 07:09:59 +0100

    WIP third challenge

--- 3/Program.fs
+++ 3/Program.fs
@@ -1,11 +1,37 @@
 ﻿open System
 
 let input = 325489 |> float
+//let input = 12 |> float
+
+let upperright x =
+    x
+    |> (fun y -> pown y 2)
+    |> (*) 2.0
+    |> (-) (2.0 * x)
+    |> (+) 1.0
+
+let computeLayer x =
+        x
+        |> sqrt
+        |> (fun x -> x - 1.0)
+        |> (fun x -> x / 2.0)
+        |> ceil
 
 [<EntryPoint>]
 let main argv =
-    let size = input |> sqrt |> ceil |> int
-    let ringMax = pown size 2
-    printfn "%i %i" size ringMax
+    let layer = computeLayer input
+
+    let result =
+        input
+        |> computeLayer
+        |> upperright
+        |> (-) input
+        |> (%) layer
+        |> abs
+        |> (+) layer
+        |> int
+    
+
+    printfn "%i" result
     System.Console.ReadKey () |> ignore
     0 // return an integer exit code
