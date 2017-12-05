035f0bf95e8d2ad46dcfdf92ef442d817b4b6130
Unknown <mail@conblem.me>
2017-12-05 07:22:19 +0100

    Fifth challenge

--- 5/Program.fs
+++ 5/Program.fs
@@ -1,29 +1,21 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open System.IO
 
-let replace (list: 'T []) index value =
-    Array.set list index value
-    list
-
-let rec iterate list index =
-    match List.tryItem index with
+let rec find index step list =
+    match Array.tryItem index list with
     | Some item -> 
-        replace(list index item)
-
-    | None ->
-
-Array.set
+        Array.set list index (item + 1)
+        find (index + item) (step + 1) list
+    | None -> step
 
 [<EntryPoint>]
 let main argv =
     let input =
-        "./input.txt"
+        "./5/input.txt"
         |> File.ReadAllLines
-        |> List.ofArray
-        |> List.map int
+        |> Array.map int
+        |> find 0 0
     
     printfn "%A" input
-    System.Console.ReadKey |> ignore
-    0 // return an integer exit code
+    System.Console.ReadKey () |> ignore
+    0
