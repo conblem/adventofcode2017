62ca8b6eed50a3233f16761b018c91329099aab2
conblem <mail@conblem.me>
2017-12-06 20:18:38 +0100

    WIP 6th challenge

--- 06 1/Program.fs
+++ 06 1/Program.fs
@@ -1,26 +1,30 @@
 ﻿open System
 open System.IO
 
-let split (x: string) = x.Split [|'\t'|] |> List.ofSeq
-
 let rec distribute index value list =
     match Array.tryItem index list with
     | _ when value = 0 -> list
     | Some item ->
-        Array.set list index (item + 1)
-        distribute (index + 1) (value - 1) list
+        let copy = Array.copy list
+        Array.set copy index (item + 1)
+        distribute (index + 1) (value - 1) copy
     | None -> distribute 0 value list
 
+let minimize index elem maxIndex =
+    if index = maxIndex
+        then 1
+        else elem
 
-let isSame = (=)
+let rec rearange original list =
+    let maxIndex = List.findIndex ((=) <| List.max list) list
+    List.mapi (minimize maxIndex) list
 
 [<EntryPoint>]
 let main argv =
     let input =
         "./input.txt"
         |> File.ReadAllText
-        |> split
-        |> List.sort
+        |> (fun x -> x.Split [|'\t'|])
 
     printfn "%A" input
     0
