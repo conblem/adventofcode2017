280f61d110b3f40c95b6b81a1052542e9af7995c
conblem <mail@conblem.me>
2017-12-06 19:45:34 +0100

    WIP 6th challenge

---
+++ 06 1/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.IO
+
+let split (x: string) = x.Split [|'\t'|] |> List.ofSeq
+
+let rec distribute index value list =
+    match Array.tryItem index list with
+    | _ when value = 0 -> list
+    | Some item ->
+        Array.set list index (item + 1)
+        distribute (index + 1) (value - 1) list
+    | None -> distribute 0 value list
+
+
+let isSame = (=)
+
+[<EntryPoint>]
+let main argv =
+    let input =
+        "./input.txt"
+        |> File.ReadAllText
+        |> split
+        |> List.sort
+
+    printfn "%A" input
+    0
