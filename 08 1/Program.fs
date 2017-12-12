2181f10aca94cc5a66692ad83d35f111c5bd8b24
conblem <mail@conblem.me>
2017-12-12 14:13:16 +0100

    8th challenge first part

---
+++ 08 1/Program.fs
@@ -0,0 +1,93 @@
+﻿open System
+open System.IO
+open System.Collections.Generic
+
+type Instruction = {
+    Register: string;
+    Mode: int -> int -> int;
+    Value: int;
+    CompareRegister: string;
+    Operator: int -> int -> bool
+    CompareValue: int;
+}
+
+let emptyInstruction () = {
+    Register="";
+    Mode=(+);
+    Value=0;
+    CompareRegister="";
+    Operator=(<=)
+    CompareValue=0
+}
+
+let mapOperator (_::_::_::_::_::operator::_) acc =
+    match operator with
+    | "<" -> {acc with Operator=(<)}
+    | ">" -> {acc with Operator=(>)}
+    | "<=" -> {acc with Operator=(<=)}
+    | ">=" -> {acc with Operator=(>=)}
+    | "==" -> {acc with Operator=(=)}
+    | "!=" -> {acc with Operator=(<>)}
+    | _ -> acc
+
+let mapNumbers (_::_::value::_::_::_::compareValue::_: string list) acc = {
+    acc with
+    Value=value |> int;
+    CompareValue=compareValue |> int
+}
+
+let mapMode (_::mode::_) acc =
+    match mode with
+    | "inc" -> {acc with Mode=(+)}
+    | "dec" -> {acc with Mode=(-)}
+    | _ -> acc
+
+let mapStrings (register::_::_::_::compareRegister::_) acc = {
+    acc with
+    Register=register;
+    CompareRegister=compareRegister
+}
+
+let compose x =
+    emptyInstruction ()
+    |> mapOperator x
+    |> mapNumbers x
+    |> mapMode x
+    |> mapStrings x
+
+let split (elem: String) = elem.Split ' ' |> List.ofArray
+
+let calculate (x: Dictionary<string, int>) elem =
+    let { Register=register;
+        Mode=mode; Value=value;
+        CompareRegister=compareRegister;
+        Operator=operator;
+        CompareValue=compareValue } = elem
+    
+    if operator x.[compareRegister] compareValue
+    then x.[register] <- mode x.[register] value 
+
+let fillDictionary (x: Dictionary<string, int>) {CompareRegister=compareRegister; Register=register} =
+    x.[register] <- 0
+    x.[compareRegister] <- 0
+
+[<EntryPoint>]
+let main argv =
+    let registers = new Dictionary<string, int>()
+
+    let input =
+        "./input.txt"
+        |> File.ReadAllLines
+        |> List.ofArray
+        |> List.map (split >> compose)
+
+    List.iter (fillDictionary registers) input
+    List.iter (calculate registers) input
+
+    let result =
+        registers
+        |> Seq.maxBy (fun x -> x.Value)
+    
+    printfn "%A" result
+    Console.ReadKey () |> ignore
+    0 // return an integer exit code
